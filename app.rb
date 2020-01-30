d4a665fba75b9211cfa424afd528653e25baf286
Irène Colonna <B00684776@essec.edu>
2020-01-30 13:26:35 +0100

    creation de la structure du projet

---
+++ app.rb
@@ -0,0 +1,12 @@
+require 'bundler'
+Bundler.require
+
+$:.unshift File.expand_path("./../lib", __FILE__)
+
+require 'app/board_case'
+require 'app/board'
+require 'game'
+require 'player'
+require 'views/show'
+
+
