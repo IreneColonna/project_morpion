2470e7a444e0c149dfad4f5f56f3e162ed90483e
Irène Colonna <B00684776@essec.edu>
2020-01-30 23:58:55 +0100

    ultime tentative

--- lib/board.rb
+++ lib/board.rb
@@ -1,22 +1,19 @@
 require 'pry'
 
-class Board
-
+class 
 	attr_accessor :L1, :L2, :L3, :L4, :L5, :L6, :L7, :a_one, :a_two, :a_three, :b_one, :b_two, :b_three, :c_one, :c_two, :c_three
 
-	#TO DO :
-    #Quand la classe s'initialize, elle doit créer 9 instances BoardCases
-    #Ces instances sont rangées dans un array/hash qui est l'attr_accessor de la classe
+	# création des 9 instances de cases... ça serait mieux dans un hash
 	def initialize 
-		@a_one = BoardCase.new("A1")
-		@a_two = BoardCase.new("A2") 
-		@a_three = BoardCase.new("A3") 
-		@b_one = BoardCase.new("B1") 
-		@b_two = BoardCase.new("B3") 
-		@b_three = BoardCase.new("B3") 
-		@c_one = BoardCase.new("C1")
-		@c_two = BoardCase.new("C2")
-		@c_three = BoardCase.new("C3") 
+		@a_one = ase.new("A1")
+		@a_two = ase.new("A2") 
+		@a_three = ase.new("A3") 
+		@b_one = ase.new("B1") 
+		@b_two = ase.new("B3") 
+		@b_three = ase.new("B3") 
+		@c_one = ase.new("C1")
+		@c_two = ase.new("C2")
+		@c_three = ase.new("C3") 
 
 		@L1 = "  ---1-----2-----3---"
 		@L2 = "A |  " + "#{@a_one.state}" + "  |  " + "#{@a_two.state}" + "  |  " + "#{@a_three.state}" + "  |"
@@ -28,11 +25,50 @@ class Board
 	end
 
 
-	def play_turn
-		#demander au bon joueur ce qu'il souhaite faire
-		his_turn = Game.current_player
-
-		#2) change la BoardCase jouée en fonction de la valeur du joueur (X ou O)
+	def play_turn(case_to_fill, value)
+		
+		while case_to_fill != ( @a_one.position || @a_two.position || @a_three.position || @b_one.position || @b_two.position || @b_three.position || @c_one.position || @c_two.position || @c_three.position)
+            puts "Entre un numéro de case valide" 
+            print "> "
+            case_to_fill = gets.chomp
+		end
+		
+		case_filled = 0 
+        while case_filled < 1
+            if case_to_fill == "A1" && @a_one.state == " "
+                @a_one.state = value
+                case_filled += 1 
+            elsif case_to_fill == "A2" && @a_two.state == " "
+                @a_two.state = value
+                case_filled += 1
+            elsif case_to_fill == "A3" && @a_three.state == " "
+                @a_three.state = value
+                case_filled += 1
+            elsif case_to_fill == "B1" && @b_one.state == " "
+                @b_one.state = value
+                case_filled += 1            
+            elsif case_to_fill == "B2" && @b_two.state == " "
+                @b_two.state = value
+                case_filled += 1
+            elsif case_to_fill == "B3" && @b_three.state == " "
+                @b_three.state = value
+                 case_filled += 1
+            elsif case_to_fill == "C1" && @c_one.state == " "
+                @c_one.state = value
+                case_filled += 1
+            elsif case_to_fill == "C2" && @c_two.state == " "
+                @c_two.state = value
+                case_filled += 1
+            elsif case_to_fill == "C3" && @c_three.state == " "
+                @c_three.state = value
+                case_filled += 1
+            else 
+                puts "Impossible : cette case est déjà occupée, entre un autre numéro :"
+                print "> "
+                case_to_fill = gets.chomp
+            end
+        end
+		#2) change la ase jouée en fonction de la valeur du joueur (X ou O)
 	  end
 	
 	  def victory?
--- lib/game.rb
+++ lib/game.rb
@@ -1,5 +1,4 @@
 
-
 class Game
 
     attr_accessor :board, :current_player, :status, :players
@@ -13,7 +12,7 @@ class Game
         puts "Quel est le nom du joueur 1 ?"
         print "> "
         name_player1 = gets.chomp
-        player1 = Player.new(name_player1.to_s, "x")
+        player1 = Player.new(name_player1, "x")
         @players << player1
         puts "le joueur #{player1.name} jouera avec les #{player1.value}"
         puts " " * 50
@@ -51,49 +50,9 @@ class Game
         print "> "
         case_to_fill = gets.chomp
 
-        while case_to_fill != "A1" || "A2" || "A3" || "B1" || "B2" || "B3" || "C1" || "C2" || "C3" 
-            puts "Entre un numéro de case valide" 
-            print "> "
-            case_to_fill = gets.chomp
-        end
    
         #remplir la case avec le symbole du joueur
-        well_done = 0 
-        while well_done < 1
-            if (case_to_fill == "A1" && @board.a_one.state == " ")
-                @board.a_one.state = @current_player.value.to_s
-                well_done += 1 
-            elsif case_to_fill == "A2" && @board.a_two.state == " "
-                @board.a_two.state = @current_player.value.to_s
-                well_done += 1
-            elsif case_to_fill == "A3" && @board.a_three.state == " "
-                @board.a_three.state = @current_player.value.to_s
-                well_done += 1
-            elsif case_to_fill == "B1" && @board.b_one.state == " "
-                @board.b_one.state = @current_player.value.to_s                        
-                well_done += 1            
-            elsif case_to_fill == "B2" && @board.b_two.state == " "
-                @board.b_two.state = @current_player.value.to_s
-                well_done += 1
-            elsif case_to_fill == "B3" && @board.b_three.state == " "
-                @board.b_three.state = "#{@current_player.value}"
-                 well_done += 1
-            elsif case_to_fill == "C1" && @board.c_one.state == " "
-                @board.c_one.state = "#{@current_player.value}"
-                well_done += 1
-            elsif case_to_fill == "C2" && @board.c_two.state == " "
-                @board.c_two.state = "#{@current_player.value}"
-                well_done += 1
-            elsif case_to_fill == "C3" && @board.c_three.state == " "
-                    @board.c_three.state = "#{@current_player.value}"
-                well_done += 1
-            else 
-                puts "Impossible : cette case est déjà occupée, entre un autre numéro :"
-                print "> "
-                case_to_fill = gets.chomp
-            end
-        end
-
+        @board.play_turn(case_to_fill, @current_player.value)
 
         #vérifier si un joueur a gagné
         if @board.a_one.state == @board.b_one.state && @board.a_one.state == @board.c_one.state
@@ -156,8 +115,6 @@ class Game
             end
         end
     end  
-    
+
 end
-
-
 
